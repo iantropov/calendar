4b60753f47f64b0b101475434c8a538a52a863bc
Ivan Antropov <antropov.ivan@gmail.com>
2014-01-14 10:21:59 +0700

    Remove remember_me option

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = login(params[:email], params[:password], params[:remember_me])
+    user = login(params[:email], params[:password])
     if user
       redirect_to root_url, :notice => t("alerts.session.logged_in")
     else
